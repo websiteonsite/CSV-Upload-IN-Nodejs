a0cd9de69c6a346330a3e46a204177c2c39f384d
websiteonsite <skillsproject111@gmail.com>
2023-07-05 14:53:30 +0530

    my first commit

---
+++ config/mongoose.js
@@ -0,0 +1,25 @@
+// IMPORTING PACKAGE
+const mongoose = require("mongoose");
+
+
+// MAKING CONNECTION 
+
+// mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+const DB = 'mongodb+srv://skillsproject111:VJnjJodxqiq6LJnz@cluster0.onjqqxt.mongodb.net/'
+
+mongoose.connect(DB).then(()=>{
+    console.log('Connection successful!');
+}).catch((err) => console.log("no connection " + err));
+
+//setting it to db
+const db = mongoose.connection;
+
+// CHECKING CONNECTION
+//if error occurs
+db.on("error", console.error.bind(console, "Error connecting to DB"));
+// when db connects successfully
+db.once("open", function(){
+    console.log("Successfully connected to DB");
+});
+
+module.exports = db;
